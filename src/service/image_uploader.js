8439e06413a7291aff28c0778c7f713cbec82561
김종환 <dondon9513@gmail.com>
2021-06-11 20:06:41 +0900

    handle image uploading

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,6 +1,19 @@
 class ImageUploader {
   async upload(file) {
-    return "file";
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "jw3c1bvg");
+    const result = await fetch(
+      // 참고사이트 https://cloudinary.com/documentation/upload_images#uploading_with_a_direct_call_to_the_rest_api
+
+      // https://api.cloudinary.com/v1_1/<cloud name>/<resource_type>/upload
+      "https://api.cloudinary.com/v1_1/drtnxojxe/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    return await result.json();
   }
 }
 
